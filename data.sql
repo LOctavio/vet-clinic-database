aa797f58e698e6c2205910770f38208e702a5c30
LOctavio <luis_ram1997@hotmail.com>
2021-09-28 11:08:10 -0500

    Remove id from inserts

--- data.sql
+++ data.sql
@@ -1,11 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-values (1, 'Agumon', DATE '2020-02-03', 0, true, 10.23);
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-values (2, 'Gabumon', DATE '2018-11-15', 2, true, 8);
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-values (3, 'Pikachu', DATE '2021-01-07', 1, false, 15.04);
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-values (4, 'Devimon', DATE '2017-05-12', 5, true, 11);
-
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
+values ('Agumon', DATE '2020-02-03', 0, true, 10.23);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
+values ('Gabumon', DATE '2018-11-15', 2, true, 8);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
+values ('Pikachu', DATE '2021-01-07', 1, false, 15.04);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
+values ('Devimon', DATE '2017-05-12', 5, true, 11);
