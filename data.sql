aa3eeb3bdfe07f14f295d0555432fe869473d3ca
LOctavio <luis_ram1997@hotmail.com>
2021-09-29 14:42:04 -0500

    Reduce code of inserts

--- data.sql
+++ data.sql
@@ -1,43 +1,28 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-values ('Agumon', DATE '2020-02-03', 0, true, 10.23);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-values ('Gabumon', DATE '2018-11-15', 2, true, 8);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-values ('Pikachu', DATE '2021-01-07', 1, false, 15.04);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-values ('Devimon', DATE '2017-05-12', 5, true, 11);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-values ('Charmander', DATE '2020-02-08', 0, false, -11);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-values ('Plantmon', DATE '2022-11-15', 2, true, -5.7);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-values ('Squirtle', DATE '1993-04-02', 3, false, -12.13);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-values ('Angemon', DATE '2005-06-12', 1, true, -45);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-values ('Boarmon', DATE '2005-06-07', 7, true, 20.4);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-values ('Blossom', DATE '1998-10-13', 3, true, 17);
+VALUES
+('Agumon', DATE '2020-02-03', 0, true, 10.23)
+('Gabumon', DATE '2018-11-15', 2, true, 8),
+('Pikachu', DATE '2021-01-07', 1, false, 15.04),
+('Devimon', DATE '2017-05-12', 5, true, 11),
+('Charmander', DATE '2020-02-08', 0, false, -11),
+('Plantmon', DATE '2022-11-15', 2, true, -5.7),
+('Squirtle', DATE '1993-04-02', 3, false, -12.13),
+('Angemon', DATE '2005-06-12', 1, true, -45),
+('Boarmon', DATE '2005-06-07', 7, true, 20.4),
+('Blossom', DATE '1998-10-13', 3, true, 17),
 
 INSERT INTO owners(full_name, age)
-values ('Sam Smith', 34);
-INSERT INTO owners(full_name, age)
-values ('Jennifer Orwell', 19);
-INSERT INTO owners(full_name, age)
-values ('Bob', 45);
-INSERT INTO owners(full_name, age)
-values ('Melody Pond', 77);
-INSERT INTO owners(full_name, age)
-values ('Dean Winchester', 14);
-INSERT INTO owners(full_name, age)
-values ('Jodie Whittaker', 38);
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14)
+('Jodie Whittaker', 38);
 
-INSERT INTO species(name)
-values ('Pokemon');
-INSERT INTO species(name)
-values ('Digimon');
+INSERT INTO species(name) VALUES ('Pokemon'), ('Digimon');
 
 BEGIN;
 UPDATE animals SET species_id = 2 WHERE name like '%mon';
